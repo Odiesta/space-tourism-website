216fce1492b20891eea99d8b73dd64360fe56868
Odiesta Shandikarona <shandikaronamade@gmail.com>
2026-04-18 17:58:53 +0800

    add swiper to crew image

--- env.d.ts
+++ env.d.ts
@@ -5,3 +5,10 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+// Allow TypeScript to recognize CSS imports
+declare module "*.css";
+
+// Specifically for Swiper if the above is too broad
+declare module "swiper/css";
+declare module "swiper/css/*";
