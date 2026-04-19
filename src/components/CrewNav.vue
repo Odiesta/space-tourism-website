f60dfac33b67496f368199a4c117a893924d23cc
Odiesta Shandikarona <shandikaronamade@gmail.com>
2026-04-19 11:47:38 +0800

    make crew page responsive on desktop and fix white screen in mobile

--- src/components/CrewNav.vue
+++ src/components/CrewNav.vue
@@ -5,7 +5,9 @@ const store = useCrewStore();
 </script>
 
 <template>
-  <ul class="flex justify-center lg:justify-start gap-4 md:gap-6 md:mt-2">
+  <ul
+    class="flex justify-center lg:justify-start gap-4 md:gap-6 md:mt-6 lg:mt-24"
+  >
     <li
       v-for="(crew, index) in store.crews"
       :key="crew.name"
@@ -16,8 +18,8 @@ const store = useCrewStore();
         :class="[
           'h-2 w-2 md:h-3 md:w-3 rounded-full transition-all',
           store.activeIndex === index
-            ? 'bg-white scale-125'
-            : 'bg-white/20 scale-100 hover:bg-white/50',
+            ? 'bg-white'
+            : 'bg-white/20 hover:bg-white/50',
         ]"
       ></div>
 
